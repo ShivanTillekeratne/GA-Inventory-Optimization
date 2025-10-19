dd29a4a9231c53a9ecc76e3172621ee47675d1f5
Ravindu Weerasinghe <ravindutharuka2001@gmail.com>
2025-10-19 23:30:24 +0530

    Update bridge.py to move example code to main

--- ga_opt/agent/bridge.py
+++ ga_opt/agent/bridge.py
@@ -19,21 +19,21 @@ def call_java_optimizer(params, jar_path=JAR_NAME):
     return stdout
     return json.loads(stdout)
 
-out = call_java_optimizer(
-            {
-          "itemTypes": [
-            {"number": 1, "width": 5.0, "height": 3.0, "price": 25.0, "quantity": 20},
-            {"number": 2, "width": 10.0, "height": 15.0, "price": 55.0, "quantity": 10},
-            {"number": 3, "width": 15.0, "height": 10.0, "price": 45.0, "quantity": 15},
-            {"number": 4, "width": 25.0, "height": 20.0, "price": 75.0, "quantity": 5}
-          ],
-          "binTypes": [
-            {"number": 1, "width": 120.0, "height": 30.0},
-            {"number": 2, "width": 500.0, "height": 50.0},
-            {"number": 3, "width": 100.0, "height": 100.0},
-            {"number": 4, "width": 200.0, "height": 200.0}
-          ]
-        }
-)
-
-print(out)
+if __name__ == "__main__":
+    out = call_java_optimizer(
+                {
+              "itemTypes": [
+                {"number": 1, "width": 5.0, "height": 3.0, "price": 25.0, "quantity": 20},
+                {"number": 2, "width": 10.0, "height": 15.0, "price": 55.0, "quantity": 10},
+                {"number": 3, "width": 15.0, "height": 10.0, "price": 45.0, "quantity": 15},
+                {"number": 4, "width": 25.0, "height": 20.0, "price": 75.0, "quantity": 5}
+              ],
+              "binTypes": [
+                {"number": 1, "width": 120.0, "height": 30.0},
+                {"number": 2, "width": 500.0, "height": 50.0},
+                {"number": 3, "width": 100.0, "height": 100.0},
+                {"number": 4, "width": 200.0, "height": 200.0}
+              ]
+            }
+    )
+    print(out)
